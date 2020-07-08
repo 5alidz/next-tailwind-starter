e19d184ff1459c0f50845dcedd9635ae3ddd1774
khaled <kzack-dev@hotmail.com>
2020-07-08 07:25:08 +0300

    refactor

--- hooks/useEventListener.ts
+++ hooks/useEventListener.ts
@@ -1,45 +1,27 @@
 // credit -> https://github.com/hswolff/youtube/blob/master/videos/react-demos/src/demo/use-event-listener.tsx
-
 import { useEffect, useRef } from 'react';
 
-export function useDocumentEvent<T extends keyof DocumentEventMap>(
-  type: T,
-  callback: (this: Document, e: DocumentEventMap[T]) => void
-) {
+type DocumentCallback<T extends keyof DocumentEventMap> = (e: DocumentEventMap[T]) => void;
+type WindowCallback<T extends keyof WindowEventMap> = (e: WindowEventMap[T]) => void;
+
+export function useDocumentEvent<T extends keyof DocumentEventMap>(type: T, callback: DocumentCallback<T>) {
   const handler = useRef(callback);
+  handler.current = callback;
 
   useEffect(() => {
-    handler.current = callback;
-  });
-
-  useEffect(() => {
-    function internalHandler(this: Document, e: DocumentEventMap[T]) {
-      handler.current.call(this, e);
-    }
+    const internalHandler = (e: DocumentEventMap[T]) => handler.current(e);
     document.addEventListener(type, internalHandler);
-    return () => {
-      document.removeEventListener(type, internalHandler);
-    };
+    return () => document.removeEventListener(type, internalHandler);
   }, [type]);
 }
 
-export function useWindowEvent<T extends keyof WindowEventMap>(
-  type: T,
-  callback: (this: Window, e: WindowEventMap[T]) => void
-) {
+export function useWindowEvent<T extends keyof WindowEventMap>(type: T, callback: WindowCallback<T>) {
   const handler = useRef(callback);
+  handler.current = callback;
 
   useEffect(() => {
-    handler.current = callback;
-  });
-
-  useEffect(() => {
-    function internalHandler(this: Window, e: WindowEventMap[T]) {
-      handler.current.call(this, e);
-    }
+    const internalHandler = (e: WindowEventMap[T]) => handler.current(e);
     window.addEventListener(type, internalHandler);
-    return () => {
-      window.removeEventListener(type, internalHandler);
-    };
+    return () => window.removeEventListener(type, internalHandler);
   }, [type]);
 }
