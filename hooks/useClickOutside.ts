ee4b5478cd76fe836202d06231ff02fb81116294
khaled <kzack-dev@hotmail.com>
2020-07-08 07:25:22 +0300

    add more hooks

---
+++ hooks/useClickOutside.ts
@@ -0,0 +1,17 @@
+// credit -> custom hooks in React Talk by Tanner Linsley
+import { useRef, MutableRefObject } from 'react';
+import { useDocumentEvent } from './useEventListener';
+
+export default function useClickOutside(
+  elementRef: MutableRefObject<HTMLElement | undefined | null>,
+  callback: (e: DocumentEventMap['click']) => void
+) {
+  const callbackRef = useRef(callback);
+  callbackRef.current = callback;
+
+  useDocumentEvent('click', (e) => {
+    if (!elementRef?.current?.contains(e.target as Node) && callbackRef.current) {
+      callbackRef.current(e);
+    }
+  });
+}
