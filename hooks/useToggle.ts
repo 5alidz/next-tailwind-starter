4873ee90d44e7e2469550348559d56ed8ebb097a
khaled <kzack-dev@hotmail.com>
2020-11-08 16:53:18 +0300

    move default hooks to its own package

--- hooks/useToggle.ts
+++
@@ -1,6 +0,0 @@
-import { useState } from 'react';
-
-export function useToggle(initialState = false) {
-  const [isTrue, setIsTrue] = useState(initialState);
-  return [isTrue, () => setIsTrue(!isTrue)];
-}
